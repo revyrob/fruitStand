9139ce0578f29132cb8f2b74f68c343a69b30f1d
Kayle <kayle@breezedigital.ca>
2024-02-26 13:17:01 -0800

    first commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navigation.scss';
+
+function Navigation() {
+  return (
+    <div className='navigation'>
+    Welcome, Kwame
+  </div>
+  )
+}
+
+export default Navigation
---
+++ src/components/SaladMaker/SaladMaker.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function SaladMaker() {
+  return(
+    <>
+      <h1 className='saladMaker'>
+        <span role="img" aria-label="salad">🥗 </span>
+          Build Your Custom Salad!
+          <span role="img" aria-label="salad"> 🥗</span>
+      </h1>
+    </>
+  )
+}
